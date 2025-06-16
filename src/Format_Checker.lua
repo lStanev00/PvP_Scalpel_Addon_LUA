76e84bc0383fbb730b08bcae22015c879c890a38
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-16 09:12:03 +0300

    Format_Checker

---
+++ src/Format_Checker.lua
@@ -0,0 +1,38 @@
+local formatChecker =  function()
+    local ratedChecker = C_PvP.DoesMatchOutcomeAffectRating();
+
+    if ratedChecker then
+        local ratedArenaChecker = C_PvP.IsRatedArena();
+        if ratedArenaChecker then
+            return "Rated Arena"
+        end
+
+        local shuffleChecker = C_PvP.IsSoloShuffle();
+        if shuffleChecker then
+            return "Solo Shuffle"
+        end
+
+        local blitzChecker = C_PvP.IsSoloRBG();
+        if blitzChecker then
+             return "Battleground Blitz"
+        end
+
+        local RBGChecker = C_PvP.IsRatedSoloRBG();
+        if RBGChecker then
+            return "Rated Battleground"
+        end
+    end
+
+    local arenaChecker = C_PvP.IsArena();
+    if arenaChecker then
+        return "Arena Skirmish"
+    end
+
+    local battlegroundChecker = C_PvP.IsBattleground();
+    if battlegroundChecker then
+        return "Random Battleground"
+    end
+
+    return "Unknown Format"
+
+end
